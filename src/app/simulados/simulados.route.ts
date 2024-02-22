6416aca7ff698e799f9e88fa80c96a9b6fb6312a
Leonardo Salgado <leosalgado2004@gmail.com>
2024-02-22 20:18:35 -0300

    Estrutura de navegação do projeto

---
+++ src/app/simulados/simulados.route.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ExamComponent } from './pages/exam/exam.component';
+
+export const routes: Routes = [
+        {path: '', redirectTo: 'home', pathMatch: 'full'},
+        {path: 'home' , component: HomeComponent},
+        {path: 'teste/:exam', component: ExamComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  })
+  export class SimuladosRoutingModule { }
