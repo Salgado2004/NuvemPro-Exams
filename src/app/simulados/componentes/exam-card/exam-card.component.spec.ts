5be1b245cbd163c34955005394d03d76d6d265ec
salgado2004 <leosalgado2004@gmail.com>
2024-06-23 19:30:10 -0300

    feat: atualização da página inicial, seleção do número de perguntas, card do exame

---
+++ src/app/simulados/componentes/exam-card/exam-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExamCardComponent } from './exam-card.component';
+
+describe('ExamCardComponent', () => {
+  let component: ExamCardComponent;
+  let fixture: ComponentFixture<ExamCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ExamCardComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ExamCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
