23882c34b64fe46a1f5a737a50f722c02625a450
salgado2004 <leosalgado2004@gmail.com>
2024-07-03 11:37:06 -0300

    feat: add error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { NotFoundPageComponent } from './shared/pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/simulados', pathMatch: 'full' },
-    { path: 'simulados', loadChildren: () => import('./simulados/simulados.module').then(m => m.SimuladosModule) }
+    { path: 'simulados', loadChildren: () => import('./simulados/simulados.module').then(m => m.SimuladosModule) },
+    { path: '**', component: NotFoundPageComponent}
    ];
--- src/app/simulados/pages/exam/exam.component.ts
+++ src/app/simulados/pages/exam/exam.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { QueryExamsService } from '../../utils/query-exams.service';
 import { Simulado } from '../../utils/simulado';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QueryExamsService } from '../../utils/query-exams.service';
 
 @Component({
   selector: 'app-exam',
@@ -13,18 +13,26 @@ export class ExamComponent {
   exam: Simulado;
   questionNumber: number;
 
-  constructor(private activatedRoute : ActivatedRoute, private query: QueryExamsService) { }
+  constructor(private activatedRoute: ActivatedRoute, private query: QueryExamsService, private router: Router) { }
 
   ngOnInit() {
+    /* Query the exam by the URL */
     this.examName = this.activatedRoute.snapshot.paramMap.get("exam");
     this.questionNumber = Number.parseInt(this.activatedRoute.snapshot.paramMap.get("questions"));
-    this.query.getExam(this.examName).then((data:Simulado) => {
-      this.exam = data;
-      if(this.questionNumber > this.exam.questions){
-        this.questionNumber = this.exam.questions;
-      }
-      if (this.questionNumber < 1){
-        this.questionNumber = 1;
+    this.query.getExam(this.examName).then((data: Simulado) => {
+      /* Check if the exam exists */
+      if (data != null) {
+        /* Load the exam questions */
+        this.exam = data;
+        if (this.questionNumber > this.exam.questions) {
+          this.questionNumber = this.exam.questions;
+        }
+        if (this.questionNumber < 1) {
+          this.questionNumber = 1;
+        }
+      } else{
+        /* Redirect to error page */
+        this.router.navigate(['not-found']);
       }
     });
   }
