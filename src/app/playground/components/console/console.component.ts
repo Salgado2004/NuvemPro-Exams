43a46de46b1966792fed6b6d9735333c662ce85d
salgado2004 <leosalgado2004@gmail.com>
2024-07-10 09:59:50 -0300

    refactor: add comments and enhance readability

--- src/app/playground/components/console/console.component.ts
+++ src/app/playground/components/console/console.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { ApiPlaygroundService } from '../../utils/api-playground.service';
 import { Execution } from '../../utils/execution';
+import { ApiPlaygroundService } from '../../utils/api-playground.service';
 
 @Component({
   selector: 'app-console',
@@ -8,66 +8,107 @@ import { Execution } from '../../utils/execution';
   styleUrl: './console.component.css'
 })
 export class ConsoleComponent {
+  consoleOutput: Element;
+  userPointer: Element;
   lastElement: Node;
 
   constructor(private api: ApiPlaygroundService) { }
 
   ngOnInit() {
-    this.focusInput()
+    this.focusInput();
+    this.userPointer = document.querySelector(".pointer");
+    this.consoleOutput = document.querySelector(".output");
   }
 
-  focusInput() {
+  /* Focus the console input */
+  focusInput(): void {
     const input = document.querySelector(".input") as HTMLElement;
     input.focus();
   }
 
-  writeOutput(data: string, user: boolean) {
-    const output = document.querySelector(".output");
-    if (this.lastElement == null) this.lastElement = output.lastChild;
-    const element = this.lastElement.cloneNode();
+  /* Create a new element with the same features */
+  createElement(target: Element, data: string): Element {
+    const element = target.cloneNode() as Element;
     element.textContent = data;
+    return element;
+  }
+
+  /* Write the user prompt */
+  writeUserPrompt(element: Element): void {
+    /* Create a new div and clone the user pointer and the element */
+    let clone = this.createElement(this.userPointer, "user [ ~ ]$");
+    let div = document.createElement("div");
+    div.style.display = "flex";
+    div.append(clone, element);
+
+    this.consoleOutput.appendChild(div);
+  }
+
+  /* Write the output in the console */
+  writeOutput(data: string, user: boolean): void {
+    if (this.lastElement == null) this.lastElement = this.consoleOutput.lastChild;
+
+    /* Create a new element */
+    const element = this.createElement(this.lastElement as Element, data);
     if (user) {
-      let div = document.createElement("div");
-      let clone = document.querySelector(".pointer").cloneNode();
-      div.style.display = "flex";
-      clone.textContent = "user [ ~ ]$";
-      div.appendChild(clone);
-      div.appendChild(element);
-      output.appendChild(div);
+      this.writeUserPrompt(element);
     } else {
-      output.appendChild(element);
+      this.consoleOutput.appendChild(element);
     }
   }
 
-  clearOutput() {
-    const output = document.querySelector(".output");
-    if (this.lastElement == null) this.lastElement = output.lastChild.cloneNode();
-    for (let i = 0; output.children.length > 0; i++) {
-      output.children.item(0).remove();
+  /* Clear the output and save the last element */
+  clearOutput(): void {
+    let o = this.consoleOutput;
+    if (!this.lastElement && o.lastChild) {
+      this.lastElement = o.lastChild.cloneNode(true);
+    }
+
+    while (o.firstChild) {
+      o.removeChild(o.firstChild);
     }
   }
 
-  send(event: KeyboardEvent) {
-    const target = event.target as HTMLInputElement;
-    const data = target.value;
+  /* Retrieve the data from the input and clear it */
+  retrieveData(target: HTMLInputElement): string {
+    let data: string = target.value;
     target.value = "";
-    if (data == "clear") {
-      this.clearOutput();
-    } else if (data.startsWith("echo")) {
-      this.writeOutput(data.slice(4), true);
-    }
-    else {
-      this.writeOutput(data, true);
-      this.api.executeCommand(data).subscribe((execution: Execution) => {
-        this.writeOutput(JSON.stringify(execution, ((chave, valor)=>{
-          if(valor === null || valor.length === 0) return undefined
-          return valor;
-        }), 2), false);
-        if(execution.error !== null){
-          let output = document.querySelector(".output").lastChild as HTMLElement;
-          output.style.color = "#e35b59"
-        }
-      });
+    return data;
+  }
+
+  /* Replacer function for JSON.stringify */
+  replacer(key: string, value: string): string {
+    if (value === null || value.length === 0) return undefined;
+    return value;
+  }
+
+  /* Execute the command via API and write the output */
+  executeCommand(data: string): void {
+    this.api.executeCommand(data).subscribe((execution: Execution) => {
+      this.writeOutput(JSON.stringify(execution, this.replacer, 2), false);
+
+      /* Sintax highlighting error messages */
+      if (execution.error !== null) {
+        let output = this.consoleOutput.lastChild as HTMLElement;
+        output.style.color = "#e35b59"
+      }
+    });
+  }
+
+  /* Handle user input */
+  send(event: KeyboardEvent): void {
+    const data = this.retrieveData(event.target as HTMLInputElement);
+    this.writeOutput(data, true);
+
+    switch (true) {
+      case (data === "clear"):
+        this.clearOutput();
+        break;
+      case (data.startsWith("echo ")):
+        this.writeOutput(data.slice(5), false);
+        break;
+      default:
+        this.executeCommand(data);
     }
   }
 }
