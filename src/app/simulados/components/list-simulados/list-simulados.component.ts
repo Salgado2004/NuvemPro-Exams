a0a27e028b169e2ba9718e8c6913ebf52ab4efad
salgado2004 <leosalgado2004@gmail.com>
2024-12-13 10:20:36 -0300

    feat: create exams filter logic
    Co-authored-by: raulbana <raul.ferreira@ufpr.br>

--- src/app/simulados/components/list-simulados/list-simulados.component.ts
+++ src/app/simulados/components/list-simulados/list-simulados.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { QueryExamsService } from '../../utils/query-exams.service';
 import { Simulado } from '../../utils/simulado';
+import { QueryExamsService } from '../../utils/query-exams.service';
 
 @Component({
   selector: 'app-list-simulados',
@@ -8,15 +8,49 @@ import { Simulado } from '../../utils/simulado';
   styleUrl: './list-simulados.component.css'
 })
 export class ListSimuladosComponent {
+  private filterSimulado = ["provider"];
   public simulados: Simulado[];
+  public activeList: Simulado[];
+  public activeFilters: { name: string, value: string }[] = [];
+  public filters: Map<string, any[]> = new Map();
   loading: boolean = true;
 
   constructor(private query: QueryExamsService) { }
 
   ngOnInit() {
-    this.query.getAvailableExams().then((data:Simulado[]) => {
+    this.query.getAvailableExams().then((data: Simulado[]) => {
       this.loading = false;
       this.simulados = data;
+      this.activeList = this.simulados;
+      for (let filter of this.filterSimulado) {
+        let key = filter as string;
+        let values = Array.from(new Set(this.simulados.map(value => value[key])));
+        this.filters.set(key, values);
+      }
+    });
+  }
+
+  applyFilter(event: Event) {
+    let target = event.target as HTMLFormElement;
+
+    let existingFilter = this.activeFilters.findIndex((f) => f.name === target.title);
+    if (existingFilter !== -1) {
+      if (!target.value || target.value === 'Todos' || target.value === '') {
+        this.activeFilters.splice(existingFilter, 1);
+      } else {
+        this.activeFilters[existingFilter].value = target.value;
+      }
+    } else {
+      if (target.value && target.value !== 'Todos' && target.value !== '') {
+        this.activeFilters.push({ name: target.title, value: target.value });
+      }
+    }
+
+
+    this.activeList = this.simulados.filter((item) => {
+      return this.activeFilters.every(
+        (f) => item[f.name] === f.value
+      );
     });
   }
 }
---
+++ src/app/simulados/utils/filter-label.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterLabelPipe } from './filter-label.pipe';
+
+describe('FilterLabelPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterLabelPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
